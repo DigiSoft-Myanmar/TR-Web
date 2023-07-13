373838b100a217b4347af2d249d510fff81c2837
Hein Htoo <heinhtoozaw99@gmail.com>
2023-07-14 02:21:53 +0630

    update user

--- pages/api/test/products.tsx
+++ pages/api/test/products.tsx
@@ -18,161 +18,164 @@ export default async function handler(
     let variableCount = 0;
     let user = await prisma.user.findFirst({
       where: {
-        role: Role.Trader,
+        phoneNum: "+959899350500",
       },
     });
-
-    for (let i = 0; i < data.length; i++) {
-      let categories = categoryData.filter((z) =>
-        data[i].categories.includes(z._id)
-      );
-      let cat = await prisma.category.findMany({
-        where: {
-          slug: {
-            in: categories.map((z) => z.slug),
-          },
-        },
-      });
-      let brandJson = brandData.find((z) => z._id === data[i].brandId);
-
-      let brand = await prisma.brand.findFirst({
-        where: {
-          name: brandJson.name,
-        },
-      });
-
-      let conditionJson = conditionData.find(
-        (z) => z._id === data[i].condition
-      );
-
-      let condition = await prisma.condition.findFirst({
-        where: {
-          name: conditionJson.name,
-        },
-      });
-
-      if (data[i].type === ProductType.Fixed) {
-        await prisma.product.create({
-          data: {
-            isPublished: data[i].isPublished,
-            categoryIds: cat.map((z) => z.id),
-            type: ProductType.Fixed,
-            tags: data[i].tags,
-            sellerId: user.id,
-            shippingInformation: data[i].shippingInfo,
-            shippingInformationMM: data[i].shippingInfoMM,
-            conditionId: condition.id,
-            imgList: data[i].imgList,
-            name: data[i].name,
-            nameMM: data[i].nameMM,
-            SKU: data[i].SKU,
-            shortDescription: data[i].productInfo,
-            shortDescriptionMM: data[i].productInfoMM,
-            regularPrice: parseInt(data[i].regularPrice),
-            stockType: data[i].stockType,
-            description: data[i].description,
-            descriptionMM: data[i].descriptionMM,
-            isFeatured: data[i].isFeatured,
-            slug: data[i].slug,
-            brandId: brand.id,
-            stockLevel: data[i].stockLevel,
-            salePrice: data[i].salePrice,
-            additionalInformation: data[i].additionalInfo,
-            additionalInformationMM: data[i].additionalInfoMM,
-            priceIndex: data[i].regularPrice,
+    if (user) {
+      for (let i = 0; i < data.length; i++) {
+        let categories = categoryData.filter((z) =>
+          data[i].categories.includes(z._id)
+        );
+        let cat = await prisma.category.findMany({
+          where: {
+            slug: {
+              in: categories.map((z) => z.slug),
+            },
           },
         });
-        fixedCount = fixedCount + 1;
-      } else {
-        let attributes = [];
-        let variations = [];
-        for (let j = 0; j < data[i].attributes.length; j++) {
-          let attr: any = await prisma.attribute.findFirst({
-            where: {
-              name: data[i].attributes[j].name,
+        let brandJson = brandData.find((z) => z._id === data[i].brandId);
+
+        let brand = await prisma.brand.findFirst({
+          where: {
+            name: brandJson.name,
+          },
+        });
+
+        let conditionJson = conditionData.find(
+          (z) => z._id === data[i].condition
+        );
+
+        let condition = await prisma.condition.findFirst({
+          where: {
+            name: conditionJson.name,
+          },
+        });
+
+        if (data[i].type === ProductType.Fixed) {
+          await prisma.product.create({
+            data: {
+              isPublished: data[i].isPublished,
+              categoryIds: cat.map((z) => z.id),
+              type: ProductType.Fixed,
+              tags: data[i].tags,
+              sellerId: user.id,
+              shippingInformation: data[i].shippingInfo,
+              shippingInformationMM: data[i].shippingInfoMM,
+              conditionId: condition.id,
+              imgList: data[i].imgList,
+              name: data[i].name,
+              nameMM: data[i].nameMM,
+              SKU: data[i].SKU,
+              shortDescription: data[i].productInfo,
+              shortDescriptionMM: data[i].productInfoMM,
+              regularPrice: parseInt(data[i].regularPrice),
+              stockType: data[i].stockType,
+              description: data[i].description,
+              descriptionMM: data[i].descriptionMM,
+              isFeatured: data[i].isFeatured,
+              slug: data[i].slug,
+              brandId: brand.id,
+              stockLevel: data[i].stockLevel,
+              salePrice: data[i].salePrice,
+              additionalInformation: data[i].additionalInfo,
+              additionalInformationMM: data[i].additionalInfoMM,
+              priceIndex: data[i].regularPrice,
             },
           });
-          attr.Term = [];
-
-          for (let k = 0; k < data[i].attributes[j].terms.length; k++) {
-            let term = await prisma.term.findFirst({
+          fixedCount = fixedCount + 1;
+        } else {
+          let attributes = [];
+          let variations = [];
+          for (let j = 0; j < data[i].attributes.length; j++) {
+            let attr: any = await prisma.attribute.findFirst({
               where: {
-                attributeId: attr.id,
-                name: data[i].attributes[j].terms[j].name,
+                name: data[i].attributes[j].name,
               },
             });
-            if (term) {
-              attr.Term.push(term);
-            } else {
-              attr.Term.push({
+            attr.Term = [];
+
+            for (let k = 0; k < data[i].attributes[j].terms.length; k++) {
+              let term = await prisma.term.findFirst({
+                where: {
+                  attributeId: attr.id,
+                  name: data[i].attributes[j].terms[j].name,
+                },
+              });
+              if (term) {
+                attr.Term.push(term);
+              } else {
+                attr.Term.push({
+                  attributeId: attr.id,
+                  name: data[i].attributes[j].terms[j].name,
+                  nameMM: data[i].attributes[j].terms[j].nameMM,
+                  value: data[i].attributes[j].terms[j].value,
+                });
+              }
+            }
+            attributes.push(attr);
+          }
+          for (let j = 0; j < data[i].variations.length; j++) {
+            let variAttributes = [];
+            for (let k = 0; k < data[i].variations[j].attributes.length; k++) {
+              let attr = await prisma.attribute.findFirst({
+                where: {
+                  name: data[i].attributes[k].name,
+                },
+              });
+              variAttributes.push({
                 attributeId: attr.id,
-                name: data[i].attributes[j].terms[j].name,
-                nameMM: data[i].attributes[j].terms[j].nameMM,
-                value: data[i].attributes[j].terms[j].value,
+                name: data[i].variations[j].attributes[k].name,
+                nameMM: data[i].variations[j].attributes[k].nameMM,
+                value: data[i].variations[j].attributes[k].value,
               });
             }
-          }
-          attributes.push(attr);
-        }
-        for (let j = 0; j < data[i].variations.length; j++) {
-          let variAttributes = [];
-          for (let k = 0; k < data[i].variations[j].attributes.length; k++) {
-            let attr = await prisma.attribute.findFirst({
-              where: {
-                name: data[i].attributes[k].name,
-              },
-            });
-            variAttributes.push({
-              attributeId: attr.id,
-              name: data[i].variations[j].attributes[k].name,
-              nameMM: data[i].variations[j].attributes[k].nameMM,
-              value: data[i].variations[j].attributes[k].value,
+            variations.push({
+              ...data[i].variations[j],
+              attributes: variAttributes,
             });
           }
-          variations.push({
-            ...data[i].variations[j],
-            attributes: variAttributes,
+
+          let priceList = sortBy(
+            variations,
+            (z: any) => z.regularPrice
+          ).reverse()[0];
+
+          await prisma.product.create({
+            data: {
+              isPublished: data[i].isPublished,
+              categoryIds: cat.map((z) => z.id),
+              type: ProductType.Variable,
+              tags: data[i].tags,
+              sellerId: user.id,
+              shippingInformation: data[i].shippingInfo,
+              shippingInformationMM: data[i].shippingInfoMM,
+              conditionId: condition.id,
+              imgList: data[i].imgList,
+              name: data[i].name,
+              nameMM: data[i].nameMM,
+              shortDescription: data[i].productInfo,
+              shortDescriptionMM: data[i].productInfoMM,
+              description: data[i].description,
+              descriptionMM: data[i].descriptionMM,
+              isFeatured: data[i].isFeatured,
+              slug: data[i].slug,
+              brandId: brand.id,
+              additionalInformation: data[i].additionalInfo,
+              additionalInformationMM: data[i].additionalInfoMM,
+              attributes: attributes,
+              variations: variations,
+              priceIndex: priceList.regularPrice,
+            },
           });
+          variableCount = variableCount + 1;
         }
-
-        let priceList = sortBy(
-          variations,
-          (z: any) => z.regularPrice
-        ).reverse()[0];
-
-        await prisma.product.create({
-          data: {
-            isPublished: data[i].isPublished,
-            categoryIds: cat.map((z) => z.id),
-            type: ProductType.Variable,
-            tags: data[i].tags,
-            sellerId: user.id,
-            shippingInformation: data[i].shippingInfo,
-            shippingInformationMM: data[i].shippingInfoMM,
-            conditionId: condition.id,
-            imgList: data[i].imgList,
-            name: data[i].name,
-            nameMM: data[i].nameMM,
-            shortDescription: data[i].productInfo,
-            shortDescriptionMM: data[i].productInfoMM,
-            description: data[i].description,
-            descriptionMM: data[i].descriptionMM,
-            isFeatured: data[i].isFeatured,
-            slug: data[i].slug,
-            brandId: brand.id,
-            additionalInformation: data[i].additionalInfo,
-            additionalInformationMM: data[i].additionalInfoMM,
-            attributes: attributes,
-            variations: variations,
-            priceIndex: priceList.regularPrice,
-          },
-        });
-        variableCount = variableCount + 1;
       }
+      return res
+        .status(200)
+        .json({ fixedCount: fixedCount, variableCount: variableCount });
+    } else {
+      return res.status(200).json({ fixedCount: 0, variableCount: 0 });
     }
-    return res
-      .status(200)
-      .json({ fixedCount: fixedCount, variableCount: variableCount });
   } catch (err) {
     console.log(err);
     return res.status(400).json(err);
